76edcb1fc96a304b4b60425d83b4fb5fa05564f2
Hieu Tran <hieutrantrung.it@gmail.com>
2024-09-07 10:06:13 +0700

    fix pydantic issue with mongo objectID

--- backend/app/models/proposal.py
+++ backend/app/models/proposal.py
@@ -1,22 +1,49 @@
-from pydantic import BaseModel, Field, ConfigDict
-from typing import List, Annotated
+from pydantic import BaseModel, Field, ConfigDict, GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import core_schema as cs
+from typing import List, Annotated, Any, Callable
 from datetime import datetime
 from bson import ObjectId
 
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
+# class PyObjectId(ObjectId):
+#     @classmethod
+#     def __get_validators__(cls):
+#         yield cls.validate
+
+#     @classmethod
+#     def validate(cls, v):
+#         if not ObjectId.is_valid(v):
+#             raise ValueError("Invalid objectid")
+#         return ObjectId(v)
+
+#     @classmethod
+#     def __get_pydantic_json_schema__(cls, core_schema: cs.CoreSchema, handler: GetJsonSchemaHandler) -> JsonSchemaValue:
+#         core_schema.update(type="string")
+
+class _ObjectIdPydanticAnnotation:
+    # Based on https://docs.pydantic.dev/latest/usage/types/custom/#handling-third-party-types.
 
     @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: Callable[[Any], cs.CoreSchema],
+    ) -> cs.CoreSchema:
+        def validate_from_str(input_value: str) -> ObjectId:
+            return ObjectId(input_value)
 
-    @classmethod
-    def __get_pydantic_json_schema__(cls, field_schema):
-        field_schema.update(type="string")
+        return cs.union_schema(
+            [
+                # check if it's an instance first before doing any further work
+                cs.is_instance_schema(ObjectId),
+                cs.no_info_plain_validator_function(validate_from_str),
+            ],
+            serialization=cs.to_string_ser_schema(),
+        )
+
+PyObjectId = Annotated[
+    ObjectId, _ObjectIdPydanticAnnotation
+]
 
 class Feature(BaseModel):
     name: str = Field(description="The name of the feature")
